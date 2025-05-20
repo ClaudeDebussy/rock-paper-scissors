17adcc539c13abe4df538e7e7c63d2f341e264f4
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-05-20 18:28:47 +0500

    Create playGame function

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ function getComputerChoice(){
         return "scissors"
     }
 }
-//console.log("Computer choice: " + getComputerChoice());
 
 function getHumanChoice(){
     let humanChoice = prompt("What will you play?").toLowerCase();
-    console.log("Human choice: " + humanChoice);
     if (humanChoice === "rock") {
         return humanChoice
     } else if (humanChoice === "paper") {
@@ -20,78 +18,97 @@ function getHumanChoice(){
         return humanChoice
     } else {
         console.log("Invalid choice");
-        //getHumanChoice();
     }
 }
-//console.log(getHumanChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound() {
+        
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
 
-function playRound(humanChoice, computerChoice) {
-    console.log("Human choice: " + humanChoice)
-    console.log("Computer choice: " + computerChoice)
-    
-    let winner = null;
-    
-    //If humanChoice is rock and computerChoice is rock, it is a tie
-    //If humanChoice is rock and computerChoice is paper, computer wins
-    //If humanChoice is rock and computerChoice is scissors, player wins
-    if (humanChoice === "rock") {
-        switch (computerChoice) {
-            case "rock":
-                winner = "No one";
-                console.log(winner + " wins.");
-                break;
-            case "paper":
-                winner = "Computer";
-                console.log(winner + " wins.");
-                break;
-            case "scissors":
-                winner = "Player";
-                console.log(winner + " wins.");
-                break;
+        console.log("Human choice: " + humanChoice)
+        console.log("Computer choice: " + computerChoice)
+
+        let winner = null;
+        
+        //If humanChoice is rock and computerChoice is rock, it is a tie
+        //If humanChoice is rock and computerChoice is paper, computer wins
+        //If humanChoice is rock and computerChoice is scissors, player wins
+        if (humanChoice === "rock") {
+            switch (computerChoice) {
+                case "rock":
+                    winner = "No one";
+                    console.log(winner + " wins.");
+                    break;
+                case "paper":
+                    winner = "Computer";
+                    console.log(winner + " wins.");
+                    break;
+                case "scissors":
+                    winner = "Player";
+                    console.log(winner + " wins.");
+                    break;
+            }
+        //If humanChoice is paper and computerChoice is rock, player wins
+        //If humanChoice is paper and computerChoice is paper, it is a tie
+        //If humanChoice is paper and computerChoice is scissors, computer wins
+        } else if (humanChoice === "paper") {
+            switch (computerChoice) {
+                case "rock":
+                    winner = "Player";
+                    console.log(winner + " wins.");
+                    break;
+                case "paper":
+                    winner = "No one";
+                    console.log(winner + " wins.");
+                    break;
+                case "scissors":
+                    winner = "Computer";
+                    console.log(winner + " wins.");
+                    break;
+            }
+        //If humanChoice is scissors and computerChoice is rock, computer wins
+        //If humanChoice is scissors and computerChoice is paper, player wins
+        //If humanChoice is scissors and computerChoice is scissors, it is a tie
+        } else if (humanChoice === "scissors") {
+            switch (computerChoice) {
+                case "rock":
+                    winner = "Computer";
+                    console.log(winner + " wins.");
+                    break;
+                case "paper":
+                    winner = "Player";
+                    console.log(winner + " wins.");
+                    break;
+                case "scissors":
+                    winner = "No one";
+                    console.log(winner + " wins.");
+                    break;
+            }
+        } else {
+            console.log("Something went wrong.")
         }
-    //If humanChoice is paper and computerChoice is rock, player wins
-    //If humanChoice is paper and computerChoice is paper, it is a tie
-    //If humanChoice is paper and computerChoice is scissors, computer wins
-    } else if (humanChoice === "paper") {
-        switch (computerChoice) {
-            case "rock":
-                winner = "Player";
-                console.log(winner + " wins.");
-                break;
-            case "paper":
-                winner = "No one";
-                console.log(winner + " wins.");
-                break;
-            case "scissors":
-                winner = "Computer";
-                console.log(winner + " wins.");
-                break;
+        if (winner === "Computer") {
+            computerScore++;
+        } else {
+            humanScore++;
         }
-    //If humanChoice is scissors and computerChoice is rock, computer wins
-    //If humanChoice is scissors and computerChoice is paper, player wins
-    //If humanChoice is scissors and computerChoice is scissors, it is a tie
-    } else if (humanChoice === "scissors") {
-        switch (computerChoice) {
-            case "rock":
-                winner = "Computer";
-                console.log(winner + " wins.");
-                break;
-            case "paper":
-                winner = "Player";
-                console.log(winner + " wins.");
-                break;
-            case "scissors":
-                winner = "No one";
-                console.log(winner + " wins.");
-                break;
-        }
+        console.log("Player has " + humanScore + " points, and computer has " + computerScore + " points.");
+    }
+
+    while (humanScore < 5 && computerScore < 5) {
+        playRound();} 
+    if (humanScore === 5) {
+        console.log("You win!")
+    } else if (computerScore === 5) {
+        console.log("You lose!");
     } else {
-        console.log("Something went wrong.")
+        console.log("Error!");
     }
-    return winner;
 }
 
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+playGame();
