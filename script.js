3236f4c7612e080f1bdf97c8fa958720bf45e5c8
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-05-20 11:08:27 +0500

    Create getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+function getComputerChoice(){
+    if ((Math.random() * 100) + 1 >= 66.66) {
+        return "rock";
+    } else if ((Math.random() * 100) + 1 >= 33.33) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
